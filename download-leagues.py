4736c89bc1c52bc72074126eb32794697eee34a5
Alexander Di Battista <adibattista@nascentdigital.com>
2014-09-21 00:31:39 -0400

    Updated download script, bulk inserts to speed up the import script

--- download-leagues.py
+++ download-leagues.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python
 from bs4 import BeautifulSoup
+from progressbar import ProgressBar
 import urllib2
-import re, os
+import re
+import os
 import argparse
 import time
-from progressbar import AnimatedMarker, Bar, BouncingBar, Counter, ETA, \
-    FileTransferSpeed, FormatLabel, Percentage, \
-    ProgressBar, ReverseBar, RotatingMarker, \
-    SimpleProgress, Timer, AdaptiveETA, AdaptiveTransferSpeed
+
 
 def chunk_report(pbar, bytes_so_far, chunk_size, total_size):
   value = float(bytes_so_far) / total_size
@@ -17,107 +16,106 @@ def chunk_report(pbar, bytes_so_far, chunk_size, total_size):
 
 
 def chunk_read(pbar, response, chunk_size=8192, report_hook=None):
-  total_size = response.headers['Content-Length']
-  total_size = int(total_size)
-  bytes_so_far = 0
+    total_size = response.headers['Content-Length']
+    total_size = int(total_size)
+    bytes_so_far = 0
 
-  while 1:
-    chunk = response.read(chunk_size)
-    bytes_so_far += len(chunk)
+    while 1:
+        chunk = response.read(chunk_size)
+        bytes_so_far += len(chunk)
 
-    if not chunk:
-       break
+        if not chunk:
+            break
 
-    if report_hook:
-       report_hook(pbar, bytes_so_far, chunk_size, total_size)
+        if report_hook:
+            report_hook(pbar, bytes_so_far, chunk_size, total_size)
 
-  return bytes_so_far
+    return bytes_so_far
 
 def getLeagueContents(url, leagueUrl, args):
-  html = getUrlContents(url + leagueUrl, args)
-  bs = BeautifulSoup(html)
+    html = getUrlContents(url + leagueUrl, args)
+    bs = BeautifulSoup(html)
 
-  tables = bs.findAll('td', valign="top")
+    tables = bs.findAll('td', valign="top")
 
-  for table in tables:
-    anchors = table.findAll("a")
+    for table in tables:
+        anchors = table.findAll("a")
 
-    folderTitles = table.find("b")
-    if folderTitles:
-      for folderTitle in folderTitles:
-        if "Data" in folderTitle:
-          folderTitle = re.sub('Data Files: ', '', folderTitle)
-          
-          if not os.path.exists("data/" + folderTitle):
-            os.makedirs("data/" + folderTitle)
+        folderTitles = table.find("b")
+        if folderTitles:
+        for folderTitle in folderTitles:
+            if "Data" in folderTitle:
+                folderTitle = re.sub('Data Files: ', '', folderTitle)
 
-    for anchor in anchors:
-      if anchor:
+            if not os.path.exists("data/" + folderTitle):
+                os.makedirs("data/" + folderTitle)
 
-        if "csv" in str(anchor['href']):
+        for anchor in anchors:
+            if anchor:
+                if "csv" in str(anchor['href']):
 
-          FirstPartOfYear = anchor['href'].split('/')[1][0:2]
-          SecondPartOfYear = anchor['href'].split('/')[1][2:]
-          
-          if "0" in str(FirstPartOfYear) or "1" in str(FirstPartOfYear):
-            FirstPartOfYear = "20" + FirstPartOfYear
-          else:
-            FirstPartOfYear = "19" + FirstPartOfYear
+                    FirstPartOfYear = anchor['href'].split('/')[1][0:2]
+                    SecondPartOfYear = anchor['href'].split('/')[1][2:]
 
-          if "0" in str(SecondPartOfYear) or "1" in str(SecondPartOfYear):
-            SecondPartOfYear = "20" + SecondPartOfYear
-          else:
-            SecondPartOfYear = "19" + SecondPartOfYear
+                    if "0" in str(FirstPartOfYear) or "1" in str(FirstPartOfYear):
+                        FirstPartOfYear = "20" + FirstPartOfYear
+                    else:
+                        FirstPartOfYear = "19" + FirstPartOfYear
 
-          path = "data/" + folderTitle + "/" + anchor.text  + "/" + FirstPartOfYear + "-" + SecondPartOfYear
+                    if "0" in str(SecondPartOfYear) or "1" in str(SecondPartOfYear):
+                        SecondPartOfYear = "20" + SecondPartOfYear
+                    else:
+                        SecondPartOfYear = "19" + SecondPartOfYear
 
-          if not os.path.exists(path):
-            os.makedirs(path)
+                    path = "data/" + folderTitle + "/" + anchor.text  + "/" + FirstPartOfYear + "-" + SecondPartOfYear
 
-          csvfilepath = path + "/" + anchor['href'].split('/')[2]
+                    if not os.path.exists(path):
+                        os.makedirs(path)
 
-          if not os.path.isfile(csvfilepath) and not os.access(csvfilepath, os.R_OK):
-            response = urllib2.urlopen(url + anchor['href'])
+                    csvfilepath = path + "/" + anchor['href'].split('/')[2]
 
-            print anchor.text  + " " + FirstPartOfYear + "-" + SecondPartOfYear + " " + csvfilepath
+                    if not os.path.isfile(csvfilepath) and not os.access(csvfilepath, os.R_OK):
+                        response = urllib2.urlopen(url + anchor['href'])
 
-            # broken download progressbar
-            # with ProgressBar(widgets=[Percentage(), Bar()], maxval=10) as progress:
-            #   chunk_read(progress, response, report_hook=chunk_report)
+                    print anchor.text  + " " + FirstPartOfYear + "-" + SecondPartOfYear + " " + csvfilepath
 
-            with open(csvfilepath, "w") as code:
-              code.write(unicode(response.read(), errors='ignore'))
+                    # broken download progressbar
+                    # with ProgressBar(widgets=[Percentage(), Bar()], maxval=10) as progress:
+                    #   chunk_read(progress, response, report_hook=chunk_report)
+
+                    with open(csvfilepath, "w") as code:
+                        code.write(unicode(response.read(), errors='ignore'))
 
 def getUrlContents(url, args):
-  if args.p:
-    proxies = {'http': 'http://142.174.134.33:8080'}
-    response = urllib2.urlopen(url, proxies=proxies)
-  else:
-    response = urllib2.urlopen(url)
+    if args.p:
+        proxies = {'http': 'http://142.174.134.33:8080'}
+        response = urllib2.urlopen(url, proxies=proxies)
+    else:
+        response = urllib2.urlopen(url)
 
-  html = response.read()
+    html = response.read()
 
-  return html
+    return html
 
 def main(url, args):
-  html = getUrlContents(url + "data.php", args)
-  bs = BeautifulSoup(html)
-  rows = bs.find("table",  cellspacing="2", border="0").findChildren(['th', 'tr'])
+    html = getUrlContents(url + "data.php", args)
+    bs = BeautifulSoup(html)
+    rows = bs.find("table",  cellspacing="2", border="0").findChildren(['th', 'tr'])
 
-  for row in rows:
-    cells = row.findChildren('td')
-    for cell in cells:
-        value = cell.a
-        if value:
-          getLeagueContents(url, value['href'], args)
+    for row in rows:
+        cells = row.findChildren('td')
+        for cell in cells:
+            value = cell.a
+            if value:
+                getLeagueContents(url, value['href'], args)
 
 
 def arguments():
-  import argparse
-  parser = argparse.ArgumentParser()
-  parser.add_argument("-p", action="store_true", default=False)
-  args = parser.parse_args()
-  return args
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", action="store_true", default=False)
+    args = parser.parse_args()
+    return args
 
 if __name__ == "__main__":
     url = "http://football-data.co.uk/"
--- import-open-football-db.py
+++ import-open-football-db.py
@@ -1,44 +1,38 @@
+from itertools import izip
+from pymongo.errors import BulkWriteError
+from pprint import pprint
 import pymongo
 import glob
 import os
-import csv, json
-from itertools import izip
+import csv
+import json
 
-client = pymongo.MongoClient()
 
-#if 'openFootball' not in client.database_names():
-db = client.open_football
+if __name__ == "__main__":
+    db = pymongo.MongoClient().open_football
 
-class UnicodeDictReader(csv.DictReader):
-    def __init__(self, f, encoding, *args, **kwargs):
-        csv.DictReader.__init__(self, f, *args, **kwargs)
-        self.encoding = encoding
-    def next(self):
-        return {
-            k.decode(self.encoding): v.decode(self.encoding)
-            for (k, v) in csv.DictReader.next(self).items()
-        }
+    print "parsing csvs"
 
-print "parsing csvs"
-for path, subdirs, files in os.walk("data/"):
-    for name in files:
-        for path, subdirs, files in os.walk("data/Belgium/Jupiler League/1995-1996/"):
-            for name in files:
-                print name + path
-                with open(os.path.join(path, name), 'rb') as csvfile:
-                    reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-                    removeHeaders = next(reader)
-                    headers = ['Div','Date','HomeTeam','AwayTeam','FTHG','FTAG','FTR','HTHG','HTAG','HTR','Referee','HS','AS','HST','AST', 'HF', 'AF', 'HC', 'AC', 'HY', 'AY', 'HR', 'AR','B365H','B365D','B365A','BWH','BWD','BWA','IWH','IWD','IWA','LBH','LBD','LBA','PSH','PSD','PSA','WHH','WHD','WHA','SJH','SJD','SJA','VCH','VCD','VCA','Bb1X2','BbMxH','BbAvH','BbMxD','BbAvD','BbMxA','BbAvA','BbOU','BbMx>2.5','BbAv>2.5','BbMx<2.5','BbAv<2.5','BbAH','BbAHh','BbMxAHH','BbAvAHH','BbMxAHA','BbAvAHA']
+    for path, subdirs, files in os.walk("data/"):
+        for name in files:
+            for path, subdirs, files in os.walk("data"):
+                for name in files:
+                    print name + path
+                    matches = db.matches_collection.initialize_ordered_bulk_op()
+                    with open(os.path.join(path, name), 'rb') as csvfile:
+                        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+                        removeHeaders = next(reader)
+                        headers = ['Div','Date','HomeTeam','AwayTeam','FTHG','FTAG','FTR','HTHG','HTAG','HTR','Referee','HS','AS','HST','AST', 'HF', 'AF', 'HC', 'AC', 'HY', 'AY', 'HR', 'AR','B365H','B365D','B365A','BWH','BWD','BWA','IWH','IWD','IWA','LBH','LBD','LBA','PSH','PSD','PSA','WHH','WHD','WHA','SJH','SJD','SJA','VCH','VCD','VCA','Bb1X2','BbMxH','BbAvH','BbMxD','BbAvD','BbMxA','BbAvA','BbOU','BbMx>2_5','BbAv>2_5','BbMx<2_5','BbAv<2_5','BbAH','BbAHh','BbMxAHH','BbAvAHH','BbMxAHA','BbAvAHA']
 
-                    for line in reader:
-                        if set(line).pop() != '':
-                            matchList = iter(line)
-                            out = dict(izip(headers, matchList))
-                            print type(out)
-                            
+                        for line in reader:
+                            if set(line).pop() != '':
+                                matchList = iter(line)
+                                match = dict(izip(headers, matchList))
 
-                            exists = db.matches_collection.find(out)
-                            if exists.count() == 0:
-                                print "doesn't exist"
-                                # matches = db.matches_collection
-                                # matches.save(matchJsonObj)
+                                exists = db.matches_collection.find(match)
+                                if exists.count() == 0:
+                                    matches.insert(match)
+                    try:
+                        matches.execute()
+                    except BulkWriteError as bwe:
+                        pprint(bwe.details)
