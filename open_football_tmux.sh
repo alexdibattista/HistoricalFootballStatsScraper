558038c46ea1ba9fab6dad664f3f6af4a9277e82
Alexander Di Battista <adibattista@nascentdigital.com>
2014-09-27 11:03:47 -0400

    tmux script

--- open_football_tmux.sh
+++ open_football_tmux.sh
@@ -1,5 +1,15 @@
-#!/bin/bash
-SESSION=$USER
+setup_tmux_layout() {
+  tmux -new-window -a -n "$1" -c "$2"
 
-tmux new-session -d -s openFootball
- 
+  tmux split-window -h
+  tmux split-window -v
+
+}
+
+open_football_project() {
+  tmux new-window -a -n "$2" -c "$1"
+
+  tmux send-keys "cd $2"
+
+  tmux split-window -h -c "$1"
+}
